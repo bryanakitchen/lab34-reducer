b6bb1a302f52e37a06dc2d41225ea7d18583ed21
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2021-02-16 18:52:19 -0500

    create disable option if no redo or undo is present. clean up code. test passes

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React, { useState, useReducer } from 'react';
+import React, { useReducer } from 'react';
 
 const initialState = {
   before: [],
@@ -13,18 +13,13 @@ function reducer(state, action) {
   
   switch(action.type) {
     case 'CLICK_UNDO':
-      return { 
-        ...state, 
-        // after: after ? [current, ...after] : [current], 
+      return {
         after: [current, ...after],
         current: before[before.length - 1],
-        // pick from previous
         before: before.slice(0, -1)
-        // removes last one
       };
     case 'CLICK_REDO':
       return { 
-        ...state, 
         before: [...before, current],
         current: after[0],
         after: after.slice(1)
@@ -34,45 +29,13 @@ function reducer(state, action) {
         ...state,
         before: [...before, current],
         current: action.payload 
-        // comes from target.value - line 83
       };
     default: 
       return state;
   }
 }
 
-// const useRecord = (init) => {
-//   const [before, setBefore] = useState([]);
-//   const [current, setCurrent] = useState(init);
-//   const [after, setAfter] = useState([]);
-
-//   const undo = () => {
-//     setAfter(after => [current, ...after]);
-//     setCurrent(before[before.length - 1]);
-//     setBefore(before => before.slice(0, -1));
-//   };
-
-//   const redo = () => {
-//     setBefore(before => [...before, current]);
-//     setCurrent(after[0]);
-//     setAfter(after => after.slice(1));
-//   };
-
-//   const record = val => {
-//     setBefore(before => [...before, current]);
-//     setCurrent(val);
-//   };
-
-//   return {
-//     undo,
-//     record,
-//     redo,
-//     current,
-//   };
-// };
-
 function App() {
-  // const { current, undo, redo, record } = useRecord('#FF0000');
   const [state, dispatch] = useReducer(reducer, initialState);
 
   const undo = () => dispatch({ type: 'CLICK_UNDO' });
@@ -80,8 +43,8 @@ function App() {
 
   return (
     <>
-      <button onClick={undo}>undo</button>
-      <button onClick={redo}>redo</button>
+      <button disabled={!state.before.length} onClick={undo}>undo</button>
+      <button disabled={!state.after.length} onClick={redo}>redo</button>
 
       <input id="RECORD" type="color" value={state.current} 
         onChange={({ target }) => dispatch({ type: target.id, payload: target.value })} />
