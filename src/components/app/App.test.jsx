54f49e41b42034b4832de667c07e309c825e3b95
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2021-02-16 15:07:00 -0500

    set up app test to render app - green

--- src/components/app/App.test.jsx
+++ src/components/app/App.test.jsx
@@ -5,7 +5,11 @@ import App from './App';
 describe('App component', () => {
   afterEach(() => cleanup());
   it('renders App', () => {
-    const { asFragment } = render(<App />);
-    expect(asFragment()).toMatchSnapshot();
+    render(<App />);
+
   });
 });
+
+// to have backgroundColor of current
+// fireEvent for onClick undo
+// fireEvent for onClick redo
