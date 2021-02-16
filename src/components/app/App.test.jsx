86efc1a783d4f4f65fd20b9cdc601becae3b80bc
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2021-02-16 15:47:51 -0500

    update test to test change of display color - red

--- src/components/app/App.test.jsx
+++ src/components/app/App.test.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { render, cleanup, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
 describe('App component', () => {
   afterEach(() => cleanup());
-  it('renders App', () => {
+  it('Changes the background color and state of undo and redo', () => {
     render(<App />);
 
+    const backgroundInput = screen.findByDisplayValue('#FF0000');
+
+    const displayDiv = screen.findByTestId('display');
+
+    fireEvent.change(backgroundInput, {
+      target: {
+        value: '#00FF00'
+      }
+    });
+    
+    expect(displayDiv).toHaveStyle({
+      backgroundColor: '#00FF00'
+    });
   });
 });
 
